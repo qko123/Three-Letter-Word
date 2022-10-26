2a0ad9464754393a46e98f0f59711fd6b9be566f
Quinn Kozak <quinn.kozak@icloud.com>
2022-10-26 09:26:39 -0500

    Initial Commit

---
+++ Three Letter Word/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Three Letter Word
+//
+//  Created by Quinn Kozak on 10/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Quinn Kozak on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
